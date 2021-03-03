a5672d3e7f7aaabba28667273961af89addc61d4
kaiete <kaiete@aragon-press.com>
2021-03-03 10:37:45 +0000

    Update signed-in/index.html, signed-in/controlpanel.js files

--- signed-in/controlpanel.js
+++ signed-in/controlpanel.js
@@ -2,3 +2,6 @@ function saveAndClose() {
     const parametersForRedir = "favouritebook=" + localStorage.getItem("favourite") + "&editbutton=" + localStorage.getItem("editbutton").toString()
     window.location.replace("https://storytime.k.vu/my/done/?" + parametersForRedir)
 }
+function deletePreferences() {
+    localStorage.clear()
+}
