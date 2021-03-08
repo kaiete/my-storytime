d6e24d7488216efb8956bdbe17632c5c3d51d819
kaiete <kaiete@aragon-press.com>
2021-03-08 09:53:10 +0000

    Delete signin.js

--- signin.js
+++
@@ -1,9 +0,0 @@
-function authuser() {
-    var storytime_signin_ok = confirm("Is it OK if you are redirected to StoryTime, to sign in?")
-    if (storytime_signin_ok == true) {
-        alert("StoryTime will guide you through the sign in process.")
-        window.location.href = "https://storytime.k.vu/my/auth"
-    } else {
-        alert("My StoryTime does not have its own sign in.\nTherefore, you need to use StoryTime to sign in.\nReload the page to use StoryTime, to sign in.")
-    }
-}
